11d479cf8705bbe31a147d0f7811cd1c33670b76
xDerrick23 <dinodidiodoro23@gmail.com>
2024-02-21 15:33:19 +0100

    Initial commit

---
+++ src/main/java/dinodidiodoro/u6s6l3/U6s6l3Application.java
@@ -0,0 +1,13 @@
+package dinodidiodoro.u6s6l3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U6s6l3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U6s6l3Application.class, args);
+	}
+
+}
---
+++ src/test/java/dinodidiodoro/u6s6l3/U6s6l3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dinodidiodoro.u6s6l3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U6s6l3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
